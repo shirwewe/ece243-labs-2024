15d1290537d5b9d8dd6502086090d479bba85570
Shirley Li <shirleyy.li@outlook.com>
2024-01-04 12:56:45 -0800

    changed comments

--- lab1/lab1/part3.s
+++ lab1/lab1/part3.s
@@ -7,9 +7,9 @@ _start:
 		movi r8, 31 /*moves 30 into r8 */
 	
 	loop:	
-		add r12, r12, r9 /* does r8+r9 and puts the sum into r10*/
-		addi r9, r9, 1
-		beq r9, r8, done
+		add r12, r12, r9 /* adds r9 to r12*/
+		addi r9, r9, 1 /*increases r9 by 1 */
+		beq r9, r8, done 
 		br loop
 	
 	done: br done
