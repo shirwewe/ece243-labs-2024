c9840c92e23fd75091e74a4e7f4b71aef42e337f
Shirley Li <shirleyy.li@outlook.com>
2024-02-23 11:32:10 -0500

    finished part 3, could use more test cases but idk if i want to

--- lab6/files/part3.c
+++ lab6/files/part3.c
@@ -1,23 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+	
+int samples_n = 80000; // audio will run for 30s
 
-// Sampled at 8Khz, mono, 32b int per sample
-
-
-int samples[];
-
-int samples_n =  240000; // audio will run for 30s
-
-
-void square_wave_gen(int *samples, int frequency){
-	float fund_period = 1 / frequency;
-	float sample_per_period = fund_period / 0.000125;
-	for (int i = 0; i < sample_per_period; i++){
-		if(i > sample_per_period / 2){
+void square_wave_gen(int *samples, double frequency){
+	int low = 1;
+	int multiplier = 1;
+	double fund_period = 1 / frequency;
+	double sample_per_period = fund_period / 0.000125;
+	for (int i = 0; i < samples_n; i++){
+		if(i < multiplier * sample_per_period/2 && low){
 			samples[i] = 0;
 		}
-		samples[i] = 0xFFFFF000;
+		else if (i == multiplier * round(sample_per_period/2) && low){
+			multiplier++;
+			low = 0;
+			samples[i] = 0x0FFFFFFF;
+		}
+		else if (i == multiplier * round(sample_per_period/2) && !(low)){
+			multiplier++;
+			low = 1;
+			samples[i] = 0;
+		}
+		else{
+			samples[i] = 0x0FFFFFFF;
+		}
+	
 	}
 }
-
 
 
 struct audio_t {
@@ -44,12 +55,62 @@ void play_audio(int *samples, int n) {
             audiop->rdata = samples[i];
         }
     }
-}	
-
-
-int main(void) {
-	square_wave_gen_30(100);
-  	play_audio(samples, samples_n);
-  	while (1);
 }
 
+
+int main(){
+    // Write C code here
+	int samples_n = 240000; // audio will run for 30s
+	int* samples; 
+	samples = (int*)calloc(samples_n, sizeof(int));
+	volatile int *SW_ptr = 0xFF200040;
+	while (1){
+		int SW_value = *SW_ptr;
+		switch(SW_value){
+			case 0b1:
+				square_wave_gen(samples, 100);
+				play_audio(samples,samples_n);
+				break;
+			case 0b10:
+				square_wave_gen(samples, 200);
+				play_audio(samples,samples_n);
+				 break;
+			case 0b100:
+				square_wave_gen(samples, 400);
+				play_audio(samples,samples_n);
+				break;
+			case 0b1000:
+				square_wave_gen(samples, 800);
+				play_audio(samples,samples_n);
+				break;
+			case 0b10000:
+				square_wave_gen(samples, 1000);
+				play_audio(samples,samples_n);
+				break;
+			case 0b100000:
+				square_wave_gen(samples, 2000);
+				play_audio(samples,samples_n);
+				break;
+			/*case 0b1000000:
+				square_wave_gen(samples, );
+				play_audio(samples,samples_n);
+				break;
+			case 0b10000000:
+				square_wave_gen(samples,);
+				play_audio(samples,samples_n);
+				break;
+			case 0b100000000:
+				square_wave_gen(samples, );
+				play_audio(samples,samples_n);
+				break;
+			case 0b1000000000:
+				square_wave_gen(samples, );
+				play_audio(samples,samples_n);
+				break;*/
+			default:
+				square_wave_gen(samples, 1000);
+				play_audio(samples,samples_n);
+				
+		}
+	}
+}
--- lab6/files/part3test.c
+++ lab6/files/part3test.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 
-
-
-int samples_n =  240000; // audio will run for 30s
+int samples_n =  80000; // audio will run for 30s
 void square_wave_gen(int *samples, double frequency){
 int low = 1;
 int multiplier = 1;
@@ -15,24 +14,46 @@ for (int i = 0; i < samples_n; i++){
 	if(i < multiplier * sample_per_period/2 && low){
 		samples[i] = 0;
 	}
-	else if (i == multiplier * sample_per_period/2 && low){
+	else if (i == multiplier * round(sample_per_period/2) && low){
 		multiplier++;
 		low = 0;
-		samples[i] = 0xFFFFF000;
+		samples[i] = 0x1;
 	}
-	else if (i == multiplier * sample_per_period/2 && !(low)){
+	else if (i == multiplier * round(sample_per_period/2) && !(low)){
 		multiplier++;
 		low = 1;
 		samples[i] = 0;
 	}
 	else{
-		samples[i] = 0xFFFFF000;
+		samples[i] = 0x1;
 	}
 	
 	}
 }
 
+// void square_wave_gen(int *samples, double frequency){
+// double fund_period = 1 / frequency;
+// double sample_per_period = fund_period / 0.000125;
+// double switching_point = sample_per_period / 2;
+// 	for(int i = 0; i < samples_n; i++){
+// 		if(i < switching_point){
+// 			samples[i] = 0;
+// 		}
+// 		else if (i > swit)
+// 	}
 
+// }
+
+int main(){
+    // Write C code here
+	int samples_n =  240000; // audio will run for 30s
+	int* samples; 
+	samples = (int*)calloc(240000, sizeof(int));
+	square_wave_gen(samples, 1700);
+    for (int i = 0; i < 1000; i++){
+        printf("%d", samples[i]);
+    }
+}
 // int samples[];
 
 
@@ -51,13 +72,3 @@ for (int i = 0; i < samples_n; i++){
 // 	}
 // }
 
-int main(){
-    // Write C code here
-	int samples_n =  240000; // audio will run for 30s
-	int* samples; 
-	samples = (int*)calloc(240000, sizeof(int));
-	square_wave_gen(samples, 100);
-    for (int i = 0; i < 1000; i++){
-        printf("%d", samples[i]);
-    }
-}
