0ae634e2a36e470d4d12f52ebad62136b8c5a0bb
Shirley Li <shirleyy.li@outlook.com>
2024-03-09 13:11:56 -0500

    finished part2

---
+++ lab7/files/part2.c
@@ -0,0 +1,212 @@
+/* This files provides address values that exist in the system */
+
+/* Cyclone V FPGA devices */
+#define LEDR_BASE             0xFF200000
+#define HEX3_HEX0_BASE        0xFF200020
+#define HEX5_HEX4_BASE        0xFF200030
+#define SW_BASE               0xFF200040
+#define KEY_BASE              0xFF200050
+#define TIMER_BASE            0xFF202000
+#define PIXEL_BUF_CTRL_BASE   0xFF203020
+#define CHAR_BUF_CTRL_BASE    0xFF203030
+
+/* VGA colors */
+#define WHITE 0xFFFF
+#define YELLOW 0xFFE0
+#define RED 0xF800
+#define GREEN 0x07E0
+#define BLUE 0x001F
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define GREY 0xC618
+#define PINK 0xFC18
+#define ORANGE 0xFC00
+
+#define ABS(x) (((x) > 0) ? (x) : -(x))
+
+/* Screen size. */
+#define RESOLUTION_X 320
+#define RESOLUTION_Y 240
+
+/* Constants for animation */
+#define BOX_LEN 2
+#define NUM_BOXES 8
+
+#define FALSE 0
+#define TRUE 1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* This program implements a line drawing algorithm */
+#define ABS(x) (((x) > 0) ? (x) : -(x))
+
+int pixel_buffer_start;
+int resolution_x, resolution_y; 	// VGA screen size
+int sizeof_pixel;						// number of bytes per pixel
+int video_m, video_n;				// number of bits in VGA y coord (m), x coord (n)
+
+void get_screen_specs(void);
+void wait_for_vsync(void);
+
+void clear_screen(void);
+void plot_pixel(int, int, short int);
+void draw_line(int, int, int, int, int);
+
+void wait_for_vsync(){
+	volatile int *pixel_ctrl_ptr = (int*)PIXEL_BUF_CTRL_BASE;
+	int status;
+	*pixel_ctrl_ptr = 1; // this will start the sync process, write 1 into the front buf
+	status = *(pixel_ctrl_ptr + 3);
+	
+	while((status & 0x01) != 0){ // polling the status bit, the status bit will turn to 0 when front buffer is done rendering
+		status = *(pixel_ctrl_ptr + 3);
+	}
+}
+
+
+void clear_screen(){
+	for (int x = 0; x < 320; x++){
+		for(int y = 0; y < 240; y++){
+			plot_pixel(x, y, 0);
+		}
+	}
+}
+
+
+void draw_line(int x0, int y0, int x1, int y1, int colour){
+	int temp, y_step;
+	int is_steep = ABS(y1 - y0) > ABS(x1 - x0);
+	if(is_steep){
+		temp = x0;
+		x0 = y0;
+		y0 = temp;
+	
+		temp = x1;
+		x1 = y1;
+		y1 = temp;
+	}
+	
+	if(x0 > x1){
+		temp = x0;
+		x0 = x1;
+		x1 = temp;
+		
+		temp = y0;
+		y0 = y1;
+		y1 = temp;
+	}
+	
+	int delta_x = x1 - x0;
+	int delta_y = ABS(y1 - y0);
+	int error = -(delta_x/2);
+	int y = y0;
+	
+	if (y < y1){
+		y_step = 1;
+	}
+	
+	else{
+		y_step = -1;
+	}
+	
+	for (int x = x0; x < x1; x++){
+		if (is_steep){
+			plot_pixel(y, x, colour);
+		}
+		else{
+			plot_pixel(x, y, colour);	
+		}
+		error = error + delta_y;
+		if(error > 0){
+			y = y + y_step;
+			error = error - delta_x;
+		}
+	}	
+}
+
+void erase_line(int x0, int y0, int x1, int y1){
+	int temp, y_step;
+	int is_steep = ABS(y1 - y0) > ABS(x1 - x0);
+	if(is_steep){
+		temp = x0;
+		x0 = y0;
+		y0 = temp;
+	
+		temp = x1;
+		x1 = y1;
+		y1 = temp;
+	}
+	
+	if(x0 > x1){
+		temp = x0;
+		x0 = x1;
+		x1 = temp;
+		
+		temp = y0;
+		y0 = y1;
+		y1 = temp;
+	}
+	
+	int delta_x = x1 - x0;
+	int delta_y = ABS(y1 - y0);
+	int error = -(delta_x/2);
+	int y = y0;
+	
+	if (y < y1){
+		y_step = 1;
+	}
+	
+	else{
+		y_step = -1;
+	}
+	
+	for (int x = x0; x < x1; x++){
+		if (is_steep){
+			plot_pixel(y, x, 0);
+		}
+		else{
+			plot_pixel(x, y, 0);	
+		}
+		error = error + delta_y;
+		if(error > 0){
+			y = y + y_step;
+			error = error - delta_x;
+		}
+	}	
+}
+
+
+
+void plot_pixel(int x, int y, short int line_color)
+{
+    *(volatile short int *)(pixel_buffer_start + (y<<10) + (x<<1)) = line_color;
+}
+
+
+
+
+int main(void)
+{
+    volatile int * pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE; // pixel controller
+
+    /* Read location of the pixel buffer from the pixel buffer controller */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clear_screen();
+	 // this line is red
+	
+	while(1){
+		for (int i =0 ; i < 240; i++){
+			draw_line(0, i, 319, i, 0xF800);
+			wait_for_vsync();
+			erase_line(0, i, 319, i);
+		}
+		for (int i = 239; i >= 0; i--){
+			draw_line(0, i, 319, i, 0xF800);
+			wait_for_vsync();
+			erase_line(0, i, 319, i);
+		}
+	}
+}
+
+
